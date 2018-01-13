169ec5519fd98b087ab4eb35aa90a1badb838768
Ivan Stanojevic <ivan@byteout.com>
2018-01-13 13:30:12 +0100

    added spinner

---
+++ main.js
@@ -0,0 +1,41 @@
+App = {
+
+    preloadImages: [
+        '/assets/contact.png',
+        '/assets/music.png',
+        '/assets/home.png'
+    ],
+
+    init: function () {
+        $(document).ready(function () {
+            App.preload(App.preloadImages).then(function (value) {
+                $('.loading').remove();
+            });
+        });
+    },
+
+    preload: function (urls) {
+
+        return new Promise(function (resolve, reject) {
+            var loadedCounter = 0;
+            var toBeLoadedNumber = urls.length;
+            urls.forEach(function (url) {
+                preloadImage(url, function () {
+                    loadedCounter++;
+                    if (loadedCounter === toBeLoadedNumber) {
+                        resolve();
+                    }
+                });
+            });
+
+            function preloadImage(url, anImageLoadedCallback) {
+                var img = new Image();
+                img.src = url;
+                img.onload = anImageLoadedCallback;
+            }
+        });
+    }
+
+};
+
+App.init();
